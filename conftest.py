a8e0e1d2cf2b95b5aa4a30a777a2b9c6098e239c
unknown <maks-panteleev-98@mail.ru>
2023-09-03 13:34:40 +0300

    added main files

---
+++ conftest.py
@@ -0,0 +1,22 @@
+import time
+from selenium.webdriver.chrome.options import Options
+import pytest
+from selenium import webdriver
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default="en",
+                     help="Choose language")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    print("\nstart browser for test..")
+    language = request.config.getoption("language")
+    # Создаем объект ChromeOptions для настройки языка
+    chrome_options = Options()
+    chrome_options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    # Инициализируем браузер с заданными опциями
+    browser = webdriver.Chrome(options=chrome_options)
+    time.sleep(5)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
