2933f80aafabd39f82076c814493013a896a1d73
unknown <maks-panteleev-98@mail.ru>
2023-10-25 11:54:21 +0300

    fixed transitions between pages

--- pages/login_page.py
+++ pages/login_page.py
@@ -11,18 +11,12 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        login_link = self.browser.find_element(*LoginPageLocators.LOGIN_LINK)
-        login_link.click()
         assert "login" in self.browser.current_url, "invalid URL"
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
-        login_link = self.browser.find_element(*LoginPageLocators.LOGIN_LINK)
-        login_link.click()
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        login_link = self.browser.find_element(*LoginPageLocators.LOGIN_LINK)
-        login_link.click()
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
